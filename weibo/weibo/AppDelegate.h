fb6dc79563ff80d06e95b036dcbb4e1ea832265d
xulx <xulx@3reality.tech>
2017-04-09 14:48:24 +0800

    初始化

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by xlx on 17/4/9.
+//  Copyright © 2017年 com.3reality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
