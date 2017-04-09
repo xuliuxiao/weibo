fde31f400a9b8030a2fbed76444f395f5f852827
xulx <xulx@3reality.tech>
2017-04-09 16:25:51 +0800

    添加子控件

--- weibo/weibo/Classes/VIews/Main/MainTabBarViewController.m
+++ weibo/weibo/Classes/VIews/Main/MainTabBarViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "MainTabBarViewController.h"
+#import "HomeTableViewController.h"
+#import "MessageTableViewController.h"
+#import "DiscoverTableViewController.h"
+#import "MeTableViewController.h"
+
 
 @interface MainTabBarViewController ()
 
@@ -16,22 +21,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self addChildViewController:[HomeTableViewController new] imageName:@"tabbar_home" titleName:@"首页"];
+    [self addChildViewController:[MessageTableViewController new] imageName:@"tabbar_message_center" titleName:@"消息"];
+    [self addChildViewController:[DiscoverTableViewController new] imageName:@"tabbar_discover" titleName:@"发现"];
+    [self addChildViewController:[MeTableViewController new] imageName:@"tabbar_profile" titleName:@"我的"];
+
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 定义一个添加子控制器的方法
+- (void)addChildViewController:(UIViewController *)childController imageName:(NSString *)imgName titleName: (NSString *)titleName{
+
+    // 设置tabbarItme的图片
+    
+  
+    childController.tabBarItem.image = [UIImage imageNamed:imgName];
+    childController.tabBarItem.selectedImage = [[UIImage imageNamed:[NSString stringWithFormat:@"%@_selected", imgName]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    // 设置文字
+    [childController.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor orangeColor]} forState:UIControlStateSelected];
+    [childController.tabBarItem setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:13]} forState:UIControlStateNormal];
+    
+    childController.title = titleName;
+    // 添加控制器
+    UINavigationController *navigationVC = [[UINavigationController alloc] initWithRootViewController:childController];
+   
+    [self addChildViewController:navigationVC];
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
